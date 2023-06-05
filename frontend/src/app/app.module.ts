895a8e141671efeb0c098a20d5d6a00c3de0b5c5
elianmgn <emagnani@fi.uba.ar>
2023-06-05 16:07:04 -0300

    header add

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CommonModule } from '@angular/common';
     CommonModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/layout/components/header/header.component.ts
+++ frontend/src/app/layout/components/header/header.component.ts
@@ -1,6 +1,19 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NavigationService } from 'app/layout/navigation.service';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.css'],
+//   // standalone: true,
+//   // imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+//   encapsulation: ViewEncapsulation.None
+// })
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -15,6 +28,10 @@ export class HeaderComponent implements OnInit {
   }
 
   toggleSideNav() {
+    this.navService.setShowNav(true);
+  }
+
+  getPage() {
     this.navService.setShowNav(true);
   }
 }
