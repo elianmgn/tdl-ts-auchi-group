68cf16b436366cf60c7cd5d42b5ef6563a544c08
chiara ciriani <cciriani@kovix-group.com>
2023-06-09 19:57:39 -0300

    feat(backend): first approach to get user transactions endpoint

--- backend/src/modules/transaction/transaction.service.ts
+++ backend/src/modules/transaction/transaction.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { Transaction } from './model';
+import { User } from '../user/model';
 
 @Injectable()
 export class TransactionService {
   constructor(private userService: UserService) {}
+
+  async getUserTransactions(username: string): Promise<Transaction[]> {
+    const userWithId = await this.userService.findOneUserByUsername(username);
+    const user: User = await User.findByPk(userWithId.id, {
+      include: [
+        {
+          model: Transaction,
+          as: 'transactions',
+          required: false,
+        },
+      ],
+    });
+    return user.transactions;
+  }
 
   async addUserTransaction(username: string, transaction: Transaction) {
     const user = await this.userService.findOneUserByUsername(username);
