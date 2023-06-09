fcd4a13782cc016f017243d2a8acc02d0179d173
chiara ciriani <cciriani@kovix-group.com>
2023-06-09 19:57:39 -0300

    feat(backend): first approach to get user transactions endpoint

--- backend/src/modules/transaction/transaction.controller.ts
+++ backend/src/modules/transaction/transaction.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { Transaction } from './model';
 
 @Controller('transactions')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
+
+  @Get(':username')
+  getUserTransactions(
+    @Param('username') username: string,
+  ): Promise<Transaction[]> {
+    return this.transactionService.getUserTransactions(username);
+  }
 
   @Post(':username')
   addUserTransaction(
