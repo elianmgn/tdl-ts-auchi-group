f6d82f221414a6968e4df64fe05cf4202398181c
chiara ciriani <cciriani@kovix-group.com>
2023-06-09 14:43:11 -0300

    feat(backend): modify user table migration to add password

--- backend/src/migrations/20230609111736-create-user-table.ts
+++ backend/src/migrations/20230609111736-create-user-table.ts
@@ -15,6 +15,10 @@ module.exports = {
         type: Sequelize.STRING,
         unique: true,
       },
+      password: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
       firstName: {
         type: Sequelize.STRING,
         field: 'first_name',
