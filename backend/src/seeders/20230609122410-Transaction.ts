7382a630c056d9b0b8c209bd6e0aa9d8ebffb542
chiara ciriani <cciriani@kovix-group.com>
2023-06-09 14:59:27 -0300

    fix(backend): fix transaction seeder

--- backend/src/seeders/20230609122410-Transaction.ts
+++ backend/src/seeders/20230609122410-Transaction.ts
@@ -5,7 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert('Transaction', [
       {
         id: 1,
-        userId: 1,
+        user_id: 1,
         description: 'Entradas Taylor',
         category: 'Entertainment',
         amount: 20000,
