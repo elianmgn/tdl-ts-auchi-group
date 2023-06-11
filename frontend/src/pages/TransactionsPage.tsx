d8fa5d49559f78e01634285329bb21f4c8da74e5
elianmgn <emagnani@fi.uba.ar>
2023-06-11 02:55:58 -0300

    transaction list feature

--- frontend/src/pages/TransactionsPage.tsx
+++ frontend/src/pages/TransactionsPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Fab } from '@mui/material';
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import TransactionList from '../components/TransactionForm/TransactionList';
 
 import TransactionForm from '../components/TransactionForm/TransactionForm';
 
@@ -17,12 +18,11 @@ export default function TransactionsPage() {
 
   return (
     <div>
-      <h1>Transactions</h1>
-      <Fab variant="extended" onClick={handleOpen}>
+      <TransactionList />
+      <Fab variant="extended" href="transactions/form">
         <ReceiptLongIcon sx={{ mr: 1 }} />
         Add
-      </Fab>
-      <TransactionForm open={open} handleClose={handleClose}/>
+      </Fab >
     </div>
   );
 }
