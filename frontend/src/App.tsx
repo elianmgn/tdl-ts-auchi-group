110159b9f03c2057ec842f9846796647ac3933da
elianmgn <emagnani@fi.uba.ar>
2023-07-03 16:24:56 -0300

    bug fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './layout/Layout';
 import AuthLayout from './layout/AuthLayout';
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import LoginForm from '../components/auth/LoginForm';
+import LoginForm from '../components/Auth/LoginForm';
 
 export default function LoginPage() {
   const navigate = useNavigate();
